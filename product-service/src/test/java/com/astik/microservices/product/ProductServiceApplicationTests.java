7b4945a1ec068b9dac87ba25cf0f6e7a129b3f58
Astik Yadav <astikyadav4@gmail.com>
2025-12-17 22:01:31 +0530

    Code Changes for Integration Test in Product Service

--- product-service/src/test/java/com/astik/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/astik/microservices/product/ProductServiceApplicationTests.java
@@ -1,13 +1,56 @@
 package com.astik.microservices.product;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // Automatically manages the JDBC URL, username, and password for Spring
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0");
 
+    @LocalServerPort
+    private Integer port;
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name": "Gaming Mouse",
+                    "description": "RGB Wireless Mouse",
+                    "price": 4500.00
+                }
+                """;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .log().all() // Prints the response to the console for easier debugging
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("name", equalTo("Gaming Mouse"))
+                .body("price", equalTo(4500.0f));
+    }
 }
